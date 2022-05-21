4509f5d9c8160d50b19f80efa8ddd58078f6045c
SUJAN KHATRI <99068384+sujansince2003@users.noreply.github.com>
2022-05-21 20:56:14 +0545

    Add files via upload

---
+++ 3rdangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("enter one angle of triangle\n");
+  scanf("%d",&a);
+  printf("enter second angle of triangle\n");
+scanf("%d",&b);
+c=180-(a+b);
+ printf("third angle of triangle is %d\n",c);
+ return 0;
+
+}
