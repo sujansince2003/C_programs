391161f3d6b68bf2ddb09c23ac6f0f374bd7f261
SUJAN KHATRI <99068384+sujansince2003@users.noreply.github.com>
2022-07-21 19:46:21 +0545

    Add files via upload

---
+++ C_programs_usingCONDITIONAL_OPERATOR/conditionalOperator_syntax.c
@@ -0,0 +1,69 @@
+
+ Expression1? expression2: expression3;  
+/*In the above syntax, the expression1 is a Boolean condition that can be either true or false value.
+If the expression1 results into a true value, then the expression2 will execute.
+The expression2 will execute if exp1 is true
+If the expression1 returns false value then the expression3 will execute.
+
+
+
+As we know that the behavior of conditional operator and 'if-else' is similar but 
+they have some differences. Let's look at their differences.
+
+1.A conditional operator is a single programming statement, while the 'if-else' statement is a programming block in which statements come under the parenthesis.
+2.A conditional operator can also be used for assigning a value to the variable, whereas
+ the 'if-else' statement cannot be used for the assignment purpose.
+3.It is not useful for executing the statements when the statements are multiple,
+ whereas the 'if-else' statement proves more suitable when executing multiple statements.
+4.The nested ternary operator is more complex and cannot be easily debugged, while the 
+nested 'if-else' statement is easy to read and maintain.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
