2e449c4144530ef6ad0c38a69690e61fe5f37e25
SUJAN KHATRI <99068384+sujansince2003@users.noreply.github.com>
2023-05-12 00:06:01 +0545

    Update Alphabet_Or_not.c

--- If-Else_Programs/Alphabet_Or_not.c
+++ If-Else_Programs/Alphabet_Or_not.c
@@ -6,17 +6,17 @@ int main()
   printf("enter the CHARACTER\n");
   scanf("%c", &ch);               // taking input from user
   if ((ch >= 'a') && (ch <= 'z')) // to be alphabet it must lies between a and z
-  // USING "" INSTEAD OF '' GIVES ERROR SO ALWAYS USE 'A','a'......etc
+  // USING "" INSTEAD OF '' GIVES ERROR SO ALWAYS USE 'A','a'.......etc
   {
     printf("this is alphabet\n");
   }
   else if ((ch >= 'A') && (ch <= 'Z')) // user also may input in capital letter so it also may lies between A and Z
   {
-    printf("This is alphabet\n");
+    printf("This is Alphabet\n");
   }
   else
   {
-    printf("This is not an alphabet\n");
+    printf("This is not an Alphabet\n");
   }
   return 0;
 } // SuzanCodes
